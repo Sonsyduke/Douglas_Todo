c3947966eb556f364c42a4bdfcab1f44fa70d45e
Douglas <douglaslombard29@gmail.com>
2022-06-15 10:22:45 +0200

    Styled the list items

--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,7 @@ function addTodo(event) {
   // Trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = "<i class='fas fa-trash'></i>";
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
 
   // Append to list
